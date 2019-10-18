c2bc129d2e4ec20118f645049e9773bc282d6671
stephaneworkspace <s.bressani@bluewin.ch>
2019-10-18 23:57:40 +0200

    debug

--- StephaneHomePage/Startup.cs
+++ StephaneHomePage/Startup.cs
@@ -53,7 +53,7 @@ namespace StephaneHomePage
                     options.HttpsPort = 443;
                 });
                 Console.WriteLine("Debug");
-                Console.WriteLine(new DirectoryInfo(@"/src/").FullName);
+                Console.WriteLine(new DirectoryInfo(@"./.aspnet/https/").FullName);
                 services.AddDataProtection()
                     .PersistKeysToFileSystem(new DirectoryInfo(@"/src/")) // \\root\.aspnet\https\
                     .ProtectKeysWithCertificate(
