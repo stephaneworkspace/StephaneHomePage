878ec778195732938aea537e00cfa15cf56cbf67
stephaneworkspace <s.bressani@bluewin.ch>
2019-10-18 22:01:31 +0200

    no https

--- StephaneHomePage/Startup.cs
+++ StephaneHomePage/Startup.cs
@@ -47,7 +47,7 @@ namespace StephaneHomePage
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                // app.UseHsts(); Don't forgot with Let's encrypt
             }
 
             app.UseHttpsRedirection();
