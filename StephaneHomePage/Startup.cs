972c57cb03e311c9ce629c3753e09e099177015b
stephaneworkspace <s.bressani@bluewin.ch>
2019-10-19 04:02:37 +0200

    test

--- StephaneHomePage/Startup.cs
+++ StephaneHomePage/Startup.cs
@@ -53,10 +53,10 @@ namespace StephaneHomePage
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
