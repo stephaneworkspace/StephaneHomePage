d08b4a90c09391398fe0408f920a85802403fdaf
stephaneworkspace <s.bressani@bluewin.ch>
2019-10-23 11:18:04 +0200

    front end

--- StephaneHomePage/Model/ThemeAstralModel.cs
+++ StephaneHomePage/Model/ThemeAstralModel.cs
@@ -9,13 +9,17 @@ namespace StephaneHomePage.Model
 {
     public class ThemeAstralModel
     {
-        [Required]
+        [Required(ErrorMessage = "Le champ « {0} » est obligatoire.")]
+        [Display(Name = "date")]
         public string year_month_day { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Le champ « {0} » est obligatoire.")]
+        [Display(Name = "hh:mm")]
         public string hour_min { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Le champ « {0} » est obligatoire.")]
+        [Display(Name = "latitude")]
         public string lat { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Le champ « {0} » est obligatoire.")]
+        [Display(Name = "longitude")]
         public string lng { get; set; }
     }
 }
