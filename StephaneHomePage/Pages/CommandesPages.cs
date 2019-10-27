2c85dcc4c9c10fc2ff1f8207f71e428e508c2b36
stephaneworkspace <s.bressani@bluewin.ch>
2019-10-27 21:01:50 +0100

    add cmd

---
+++ StephaneHomePage/Pages/CommandesPages.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using StephaneHomePage.Services.Core;
+using StephaneHomePage.Struct.Layout;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StephaneHomePage.Pages
+{
+    public partial class CommandesPages
+    {
+        [Inject]
+        public AppStateServiceCore AppStateServiceCore { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            var oldStruct = AppStateServiceCore.AppBarStruct;
+            var newStruct = new AppBarStruct("Commandes", false, oldStruct.AstroZoom, oldStruct.LockBook);
+            await AppStateServiceCore.ChangeStruct(newStruct);
+        }
+    }
+}
