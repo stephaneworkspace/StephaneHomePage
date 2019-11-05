7845a73806689f87f8dbcc1b196782217786aac8
stephaneworkspace <s.bressani@bluewin.ch>
2019-11-06 00:17:12 +0100

    Ajout de vi

---
+++ StephaneHomePage/Pages/ViPages.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using StephaneHomePage.Services.Core;
+using StephaneHomePage.Struct.Layout;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StephaneHomePage.Pages
+{
+    public partial class VimPages
+    {
+        [Inject]
+        public AppStateServiceCore AppStateServiceCore { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            var oldStruct = AppStateServiceCore.AppBarStruct;
+            var newStruct = new AppBarStruct("Commandes vim", false, oldStruct.AstroZoom, oldStruct.LockBook);
+            await AppStateServiceCore.ChangeStruct(newStruct);
+        }
+    }
+}
